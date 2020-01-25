7e76ce2bcb06b5486edc2e443bd7d8d5e7834a90
akhil <akhilkhubchandani@gmail.com>
2020-01-25 17:52:07 +0530

    gui, , About and added scroll

--- js/main.js
+++ js/main.js
@@ -7,3 +7,29 @@ $(function () {
         }
     });
 });
+
+//for smooth scrolling
+// $(document).ready(function(){
+//    $("a[href^='#'").on("click",function(e) {
+//        e.preventDefault();
+//        var offset=100;
+//        return $("html","body").animate({
+//            scrollTop: $(this.hash).offset().top-offset
+//        },300);
+//        });
+// });
+
+$(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+  
+        var target = this.hash,
+        $target = $(target);
+  
+        $('html, body').animate({
+            'scrollTop': $target.offset().top-80
+        }, 900, 'swing', function () {
+           
+        });
+    });
+  });
