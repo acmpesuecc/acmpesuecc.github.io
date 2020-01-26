864114c40f7636f36506316947a2f55a1940b1bd
hemantsathish <56361843+hemantsathish@users.noreply.github.com>
2020-01-26 19:49:26 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ $(document).ready(function(){
   
         $('html, body').animate({
             'scrollTop': $target.offset().top-80
-        }, 900, 'swing', function () {
+        }, 350, 'swing', function () {
            
         });
     });
