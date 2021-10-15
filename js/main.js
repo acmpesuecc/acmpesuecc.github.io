828ffc2a621d9e108bc510024c3e57e9cffe6db2
Shreya Ajith <shreya.ajithchb@gmail.com>
2021-10-15 19:55:47 +0530

    fixed the redundant var

--- js/main.js
+++ js/main.js
@@ -14,7 +14,6 @@ $(function () {
 $(document).ready(function(){
     $('a[href^="#"]').on('click',function (e) {
         e.preventDefault();
-  
         var target = this.hash,
         if($(target).length){
             $('html, body').animate({
