1ca83e2d277a36b1c7e1b1bf08d3a91ae9a03d92
Shreyas Kaundinya <shreyassk08@gmail.com>
2021-10-15 19:50:35 +0530

    Fixes lazy loading of images

--- js/lazyload.js
+++ js/lazyload.js
@@ -1,35 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let lazyloadImages = document.querySelectorAll("img.lazy");
-    lazyloadImages = Array.from(lazyloadImages);
-    let lazyloadThrottleTimeout;
+  var lazyImages = document.querySelectorAll("img.lazy");
 
-    function lazyload() {
-        if (lazyloadThrottleTimeout) {
-            // if already timeour exists
-            clearTimeout(lazyloadThrottleTimeout);
+  if ("IntersectionObserver" in window) {
+    let lazyImageObserver = new IntersectionObserver(function (entries) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          let img = entry.target;
+          img.src = img.dataset.src;
+          img.classList.remove("lazy");
+          lazyImageObserver.unobserve(img);
         }
+      });
+    });
 
-        function loadImages() {
-            var scrollTop = window.pageYOffset;
-            lazyloadImages.forEach(function (img, index) {
-                if (img.offsetTop < window.innerHeight + scrollTop) {
-                    img.src = img.dataset.src;
-                    img.classList.remove("lazy");
-                    lazyloadImages.splice(index, 1); // removes the image to prevent further calls to lazy load for this
-                }
-            });
-            if (lazyloadImages.length == 0) {
-                document.removeEventListener("scroll", lazyload);
-                window.removeEventListener("resize", lazyload);
-                window.removeEventListener("orientationChange", lazyload);
-            }
-        }
-
-        lazyloadThrottleTimeout = setTimeout(loadImages, 50);
-    }
-
-    //add event listeners throughout the page
-    document.addEventListener("scroll", lazyload);
-    window.addEventListener("resize", lazyload);
-    window.addEventListener("orientationChange", lazyload);
+    lazyImages.forEach(function (img) {
+      lazyImageObserver.observe(img);
+    });
+  } else {
+    // if intersection observer doesnt exist on browser
+    // load images at once
+    lazyImages.forEach((img) => {
+      img.src = img.dataset.src;
+      img.classList.remove("lazy");
+    });
+  }
 });
