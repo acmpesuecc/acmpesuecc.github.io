cfd020e07ea29f7428db4434a6d1c00cdf6565a2
Achyuth Sosale <achyuthyogesh0@gmail.com>
2024-02-13 23:20:50 +0530

    Adds Navbar and seedha sadha designs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
-
 export const metadata: Metadata = {
-  title: 'ACM ~ PESUECC',
+  title: 'ACM | PESUECC',
   // Need a shorter and better description
   description: `The ACM Student Chapter here at PES University Electronic City
             Campus brings you a world of opportunities, events and contains a
@@ -25,9 +22,53 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/acm_logo.ico" />
+        <link rel="icon" href="/acmshortlogo.png" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className=" mx-auto max-w-[90%]">
+        {/* Trimming body and placing in the center */}
+        <nav className="mx-auto mt-6  flex h-[30%] w-[100%] flex-col lg:flex-row lg:items-center lg:justify-between">
+          {/* Nav switches between row and column display for phone and mobile*/}
+          <a
+            href="/"
+            className="mx-auto w-[30%] hover:cursor-pointer lg:mx-0 lg:w-[8%]"
+          >
+            {/* Smol ACM logo as home button */}
+            <img
+              src="/acmshortlogo.png"
+              alt="acmshortlogo"
+              className="h-full w-full"
+            />
+          </a>
+          {/* Nav routes spaced out */}
+          <div className="options mx-auto mt-4 flex max-w-[30%] flex-row justify-center space-x-6 lg:mx-0 lg:mt-0 lg:items-center lg:justify-normal lg:space-x-10">
+            <a
+              href="/about"
+              className=" text-center text-xl text-white decoration-sky-200 hover:cursor-pointer hover:underline hover:underline-offset-8 lg:text-3xl"
+            >
+              About
+            </a>
+            <a
+              href="/events"
+              className=" text-center text-xl text-white decoration-sky-200 hover:cursor-pointer hover:underline hover:underline-offset-8 lg:text-3xl"
+            >
+              Events
+            </a>
+            <a
+              href="/blog"
+              className=" text-center text-xl text-white decoration-sky-200 hover:cursor-pointer hover:underline hover:underline-offset-8 lg:text-3xl"
+            >
+              Blog
+            </a>
+            <a
+              href="/team"
+              className=" text-center text-xl text-white decoration-sky-200 hover:cursor-pointer hover:underline hover:underline-offset-8 lg:text-3xl"
+            >
+              Team
+            </a>
+          </div>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,11 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <>
-      <img src="/acm_big_logo.png" alt="ACM Logo" />
+      <img
+        src="/acm_big_logo.png"
+        alt="ACM Logo"
+        className="mx-auto mt-20 w-[90%] lg:mt-32 lg:w-2/3 "
+      />
     </>
   );
 }
