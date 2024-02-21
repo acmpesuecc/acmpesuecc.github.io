b7d32d7d133a0dbd7df6ca2e8e4570891d5444ff
Achyuth Sosale <achyuthyogesh0@gmail.com>
2024-02-21 11:03:16 +0530

    Config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '/';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+const nextConfig = {
+  assetPrefix: assetPrefix,
+  basePath: basePath
+};
 
 export default nextConfig;
