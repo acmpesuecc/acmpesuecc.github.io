96a54e6408257859414a47bac1373e22a3c52d5e
Delta18-Git <64347927+Delta18-Git@users.noreply.github.com>
2026-02-18 20:38:04 +0530

    feat: add 2026 AIEP projects to the project list

--- app/aiep/data/projects.tsx
+++ app/aiep/data/projects.tsx
@@ -13,6 +13,114 @@ export interface Project {
 
 export const projects: Project[] = [
   {
+    year: 2026,
+    slug: 'literate',
+    title: 'Literate: A cross-platform, BYO-Cloud PDF Reader & Sync Engine PWA',
+    leads: ['Arjun D Kumar', 'Hemanth Andey'],
+    members: [
+      'Anna Maria Cijo',
+      'Avantika Jayaraja Puthran',
+      'C K Gagan Gowda',
+      'Harshul Agarwal',
+      'Krithik V',
+      'S Prithika'
+    ],
+    desc: `Literate is a project built aiming to make reading books and PDFs easier, more accessible, and open to change - by the people, for the people and of the people
+    Being a PWA Literate enables users to save their bookmarks during their read on their hectic travel, to accessing the same bookmarks at the comfort of their beds in their laptops or tablets`
+  },
+  {
+    year: 2026,
+    slug: 'kinemation',
+    title: 'kinemation: A computer vision ML pipeline',
+    leads: ['Maaya Mohan'],
+    members: [
+      'Amrita Pradeep',
+      'Hemaksh Breja',
+      'Kurias Joji',
+      'Navya S Gurupadmath',
+      'Saisree Vaishnavi',
+      'Yatin R'
+    ],
+    desc: `Kinemation is a computer vision project that takes a regular video of a person and turns their movements into animated stick figures. It works by identifying important points on the body and using thento form a simple skeletal version of the person. The result is a minimal, clean stick figure that mirrors the original motion in the video.
+    The idea is not just to draw stick figures, but to make them move in a way that actually feels natural and smooth. Even when the video has fast movements or awkward angles, the goal is to keep the animation stable and easy to understand. In the end, Kinemation is about capturing real human motion and expressing it in the simplest and most fun visual form possible.`
+  },
+  {
+    year: 2026,
+    slug: 'cirem',
+    title: 'CIReM: An experimental ML research project',
+    leads: ['Lakshit Talreja', 'Hemanth Andey'],
+    members: [
+      'Arjun Mohan Gowda',
+      'Atreya B Deshpande',
+      'Prabodh Bharadwaj',
+      'Laxmi Karthikeya Gollapudi',
+      'Shivangi Tiwari'
+    ],
+    desc: `CIReM, a project that...
+    ... doesn’t quite know how it got its name.
+    Entered as a slightly experimental setup, CIReM aims to see if a group of highly motivated students can make it into the research domain without a specific mentor guidance that's usually present.
+    The team of 4 aims to analyse and find commonality among the 4 methods used to regularise Representation Models (Autoencoders), and to help speed up training of World Models effectively.`
+  },
+  {
+    year: 2026,
+    slug: 'awasme',
+    title: 'aWASMe: A WASM interpreter',
+    leads: ['Nirupama Jayaraman', 'Sarah Kazi', 'Selvaganesh Arunmozhi'],
+    members: [
+      "Aston Venol D'Souza",
+      'Chitra Likith Kumar',
+      'Samyuktha Ramesh Babu',
+      'Shashidharan VS'
+    ],
+    desc: `A WASM interpreter with dual modes — one as a browser-based VM and one as a simulator for WASM. It uses a structured parsing and execution pipeline in C++ & emscripten, to load binary modules , validate and decode instructions, and simulate a full virtual machine directly in the browser. It is a very useful tool when it comes to experimenting on systems safely and also helps out to build/test/debug any program in a sandboxed environment without requiring external runtimes.`
+  },
+  {
+    year: 2026,
+    slug: 'progchamp',
+    title: 'ProgChamp',
+    leads: ['Ashmita Chaki', 'Rehaan Jose Mathew', 'Zia Kadijah'],
+    members: [
+      'Archita Agrawal',
+      'Poornaprajna Kashyap',
+      'Pratham Mudakavi',
+      'Sannidhi Nayak',
+      'Syed Ayaan Hasan'
+    ],
+    desc: `ProgChamp is an online platform for users to publish and enjoy a collection of various games, sortable by genre and carefully curated. Users can rate the games, comment on them, and to show some extra love, they can even boost the games’ popularity by superliking up to 3 of them! To spice things up, ProgChamp will also be hosting seasonal challenges that aspiring game developers can join and compete in. It can potentially be expanded beyond games to other user-generated content as well :D`
+  },
+  {
+    year: 2026,
+    slug: 'radfs',
+    title: 'radFS: In-memory FUSE filesystem built using adaptive radix trees',
+    leads: ['Pranav V Bhat', 'Vinaayak G Dasika'],
+    members: [
+      'Angelo Arakal',
+      'Bhuvigna Reddy A T',
+      'M C Nirmal Kumar',
+      'Saankhya Srikanth'
+    ],
+    desc: `Prioritizing memory density over raw speed, radFS challenges the team to build a space-efficient filesystem using FUSE and Go. By using Adaptive Radix Trees (ART) instead of B-Trees as the main storage data structure, the team aims to try and solve the problem of worst-case space consumption while exploring the safety and flexibility of userspace development.`
+  },
+  {
+    year: 2026,
+    slug: 'opencms',
+    title:
+      'OpenCMS: A minimal headless CMS for blog publishing, designed to integrate with SSGs',
+    leads: ['Mebin J Thattil', 'Sabique Islam'],
+    members: ['Kalyani Puranik', 'Manav Dewangan', 'Prarthana Vadeesha'],
+    desc: `OpenCMS is a minimal headless CMS for publishing blogs directly from a git repository, its built to integrate with hugo, zola, and other static site generators. It provides a markdown editor with live preview and asset uploads. The CMS commits edits back to github automatically, exposes APIs for raw content, metadata, and can trigger site builds through webhooks.`
+  },
+  {
+    year: 2026,
+    slug: 'scibits',
+    title: 'SciBits: A hardware implementation of a Scientific Calculator',
+    leads: ['Keval Pattani', 'Pranav M'],
+    members: ['Pranav K'],
+    desc: `A hardware-centric project aiming to design a Scientific Calculator using Verilog (RTL). The calculator will support basic arithmetic operations alongside advanced features like statistical calculations and polynomial equation solving.
+    The design follows a behavioral approach for all computational operations, with a Display module developed in parallel to visualize calculations. An Instruction Set Architecture (ISA) will be integrated into the core CPU to load sprites into the Display Module and handle computation tasks.
+    Upon completing the calculator and display functionalities, the design will be processed through the open-source ASIC implementation tool OpenLane, transforming it into an ASIC-ready chip.`
+  },
+  {
     year: 2024,
     slug: 'retro-game-console-emulation',
     title: 'Retro Game Console Emulation: Write Your Own CHIP8 Interpreter!',
@@ -47,7 +155,7 @@ export const projects: Project[] = [
   {
     year: 2024,
     slug: 'exploring-database-internals',
-    title: 'Exploring Database Internals(by trying to build one!)',
+    title: 'Exploring Database Internals (by trying to build one!)',
     leads: ['Siddharth Tewari'],
     members: [
       'Saijyoti P',
